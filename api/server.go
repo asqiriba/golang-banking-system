cad8c39d8884a36c67c5605f549dbeb07327ef1a
asqiriba <hassan@asqiriba.com>
2022-08-13 17:36:34 -0700

    add api routes

---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	db "github.com/asqiriba/golang-banking-system/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// Create a new server instance.
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Runs the server on the given address.
+func (server *Server) Start(addr string) error {
+	return server.router.Run(addr)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{
+		"error": err.Error(),
+	}
+}
