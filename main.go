277c22119683bb2e8d27548a05eebd49066d6417
asqiriba <hassan@asqiriba.com>
2022-08-13 18:02:30 -0700

    add env file and `viper`

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/asqiriba/golang-banking-system/api"
 	db "github.com/asqiriba/golang-banking-system/db/sqlc"
+	"github.com/asqiriba/golang-banking-system/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Failed to open database ", err)
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Failed to start server ", err)
 	}
