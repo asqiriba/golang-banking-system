bcfd8f8db251a52c8b9716a90fd7a4ebd970e609
asqiriba <hassan@asqiriba.com>
2022-08-17 22:16:37 -0700

    add sqlc code for `users`

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -31,3 +31,12 @@ type Transfer struct {
 	Amount    int64
 	CreatedAt time.Time
 }
+
+type User struct {
+	Username          string
+	HashedPassword    string
+	FullName          string
+	Email             string
+	PasswordChangedAt time.Time
+	CreatedAt         time.Time
+}
